c327f7b796496aaff0e0ce492c07e12704d0df3f
Thomas Madlener <thomas.madlener@desy.de>
2021-04-20 18:17:58 +0200

    Add collection id to outputs

--- python/prototyping/podio/collection.py
+++ python/prototyping/podio/collection.py
@@ -29,30 +29,41 @@ class CollectionBase:
   logger = logging.getLogger(f'{__name__}.CollectionBase')
   def __init__(self) -> None:
     """By default all collections are 'invalid'"""
-    self.logger.info('__init__')
+    self.logger.info(f'__init__, id: {id(self)}')
     self.buffers: CollectionBuffers = CollectionBuffers()
     self.valid = False
     self.resolved = False
+    self.prepared = True
 
   def set_buffer(self, data: DataBuffer) -> None:
     """Set the data buffer"""
-    self.logger.info('set_buffer')
+    self.logger.info(f'set_buffer, id: {id(self)}')
     self.buffers.data = data
 
   def set_ref_colls(self, ref_colls: RefCollBuffers=[]) -> None:
     """Set the ref coll buffer"""
-    self.logger.info('set_ref_colls')
+    self.logger.info(f'set_ref_colls, id: {id(self)}')
     self.buffers.ref_colls = ref_colls
 
   def set_vec_mems(self, vec_mems: VecMemBuffers=[]) -> None:
     """Set the vector members"""
-    self.logger.info('set_vec_mems')
+    self.logger.info(f'set_vec_mems, id: {id(self)}')
     self.buffers.vec_mems = vec_mems
 
   def prepare_after_read(self) -> None:
     """Prepare after read"""
     self.valid = self.buffers.valid_buffers()
-    self.logger.info(f'prepare_after_read (valid={self.valid})')
+    self.logger.info(f'prepare_after_read (valid={self.valid}), id: {id(self)}')
+
+  def prepare_for_write(self) -> None:
+    """Prepare for write"""
+    self.logger.info(f'prepare_for_write, id: {id(self)}')
+    self.prepared = True
+    # To differentiate them from the unset default init simply fill them with
+    # some values or set to the empty list at least
+    self.buffers.data = DataBuffer(42)
+    self.buffers.ref_colls = [RefCollBuffer(3), RefCollBuffer(14), RefCollBuffer(15)]
+    self.buffers.vec_mems = []
 
   def set_references(self) -> None:
     """Set the references"""
