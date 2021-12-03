6c953f4c8cc5ebd15f15e1c3730acb5002d99ac8
Thomas Madlener <thomas.madlener@desy.de>
2021-12-03 10:19:48 +0100

    Disable Memory sanitizer in CI

--- tests/CTestCustom.cmake
+++ tests/CTestCustom.cmake
@@ -34,7 +34,7 @@ if ((NOT "@FORCE_RUN_ALL_TESTS@" STREQUAL "ON") AND (NOT "@USE_SANITIZER@" STREQ
     pyunittest
   )
 
-  # ostream_operator is working with Memory sanitizer
+  # ostream_operator is working with Memory sanitizer (at least locally)
   if("@USE_SANITIZER@" MATCHES "Memory(WithOrigin)?")
     list(REMOVE_ITEM CTEST_CUSTOM_TESTS_IGNORE ostream_operator)
   endif()
