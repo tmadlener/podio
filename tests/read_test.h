fe9cd668691f61507d2c92699413b7c9d1c654bd
Thomas Madlener <thomas.madlener@desy.de>
2022-04-07 17:42:15 +0200

    Add mutexes guarding the internal maps for GenericParameters
    - Make them non-copyable
    - Fix read_tests to use const references instead of copying

--- tests/read_test.h
+++ tests/read_test.h
@@ -41,7 +41,7 @@ bool check_fixed_width_value(FixedWidthT actual, FixedWidthT expected, const std
 
 void processEvent(podio::EventStore& store, int eventNum, podio::version::Version fileVersion) {
 
-  auto evtMD = store.getEventMetaData();
+  const auto& evtMD = store.getEventMetaData();
   auto evtWeight = evtMD.getValue<float>("UserEventWeight");
   if (evtWeight != (float)100. * eventNum) {
     std::cout << " read UserEventWeight: " << evtWeight << " - expected : " << (float)100. * eventNum << std::endl;
@@ -89,7 +89,7 @@ void processEvent(podio::EventStore& store, int eventNum, podio::version::Versio
 
   // read collection meta data
   auto& hits = store.get<ExampleHitCollection>("hits");
-  auto colMD = store.getCollectionMetaData(hits.getID());
+  const auto& colMD = store.getCollectionMetaData(hits.getID());
   const auto& es = colMD.getValue<std::string>("CellIDEncodingString");
   if (es != std::string("system:8,barrel:3,layer:6,slice:5,x:-16,y:-16")) {
     std::cout << " meta data from collection 'hits' with id = " << hits.getID() << " read CellIDEncodingString: " << es
